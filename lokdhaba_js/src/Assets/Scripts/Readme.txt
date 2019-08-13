734f7edb216d126ffbc1b5563a3c2b8c891945c6
Mohit Kumar <mohitkumar@Mohits-MacBook-Pro-2.local>
2019-08-13 15:15:07 +0530

    added files for select input options in browse data

---
+++ lokdhaba_js/src/Assets/Scripts/Readme.txt
@@ -0,0 +1,3 @@
+The code Update_Data.R updates the data in ../Data folder from tcpd_data local repository. Just give the location of your repository as an argument.
+$ Rscript Update_Data.R <location to tcpd_data.git
+
---
+++ lokdhaba_js/src/Assets/Scripts/Update_Data.R
@@ -0,0 +1,27 @@
+library(data.table)
+library(jsonlite)
+library(dplyr)
+args = commandArgs(TRUE)
+
+
+if (length(args) != 1)  {
+  stop ("Please provide 1 argument:  Directory of the tcpd_data.git")
+}
+
+
+dir <- args[1]
+
+ls_no_file = paste(dir,"GE/Data/derived/mastersheet.csv",sep="/")
+st_codes_file = paste(dir,"state_codes.csv",sep="/")
+vs_no_file = paste(dir,"AE/VidhaSabhaNumber.csv",sep="/")
+
+ls_no = fread(ls_no_file,na="")
+ls_yr = ls_no[Poll_No==0,list(Year = unique(Year)),by=c("Assembly_No","State_Name")]
+ls_yr %>% toJSON() %>% writeLines("../Data/LokSabhaNumber.json")
+
+st_codes = fread(st_codes_file,na="")
+st_codes %>% toJSON() %>% writeLines("../Data/StateCodes.json")
+
+vs_no = fread(vs_no_file,na="")
+vs_no$key_year = NULL
+vs_no %>% toJSON() %>% writeLines("../Data/VidhanSabhaNumber.json")

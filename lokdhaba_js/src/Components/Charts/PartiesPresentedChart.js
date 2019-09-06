22703f4611d22f06dd5bed476279c20bb94af83c
Mohit Kumar <mohitkumar@Mohits-MacBook-Pro-2.local>
2019-09-06 12:11:01 +0530

    created updated plotly component for parties presented and contested chart

--- lokdhaba_js/src/Components/Charts/PartiesPresentedChart.js
+++ lokdhaba_js/src/Components/Charts/PartiesPresentedChart.js
@@ -3,49 +3,40 @@ import createPlotlyComponent from 'react-plotlyjs';
 import Plotly from 'plotly.js/dist/plotly-cartesian';
 const PlotlyComponent = createPlotlyComponent(Plotly);
 
-export default class VoterTurnoutChart extends Component {
+export default class PartiesPresentedChart extends Component {
   render() {
     var vizData = this.props.data;
     var dataFilterOptions = this.props.dataFilterOptions;
     var x_labels = vizData.map(function(item){return item.Year +" (#" + item.Assembly_No + ")"});
     var data = [];
-    if(dataFilterOptions.has("male")){
-      var y_male = vizData.map(x => x.male);
+    if(dataFilterOptions.has("Parties_Represented")){
+      var y_represented = vizData.map(x => x.Parties_Represented);
       var trace = {
                     type: 'bar',
                     x: x_labels,
-                    y: y_male,
-                    name: 'male'
+                    y: y_represented,
+                    name: 'Parties Represented'
                   }
       data.push(trace);
     }
-    if(dataFilterOptions.has("female")){
-      var y_female = vizData.map(x => x.female);
+    if(dataFilterOptions.has("Parties_Contested")){
+      var y_contested = vizData.map(x => x.Parties_Contested);
       var trace = {
                     type: 'bar',
                     x: x_labels,
-                    y: y_female,
-                    name: 'female'
+                    y: y_contested,
+                    name: 'Parties Contested'
                   }
       data.push(trace);
     }
-    if(dataFilterOptions.has("total")){
-      var y_total = vizData.map(x => x.total);
-      var trace = {
-                    type: 'bar',
-                    x: x_labels,
-                    y: y_total,
-                    name: 'total'
-                  }
-      data.push(trace);
-    }
+
     let layout = {
-      title: 'Voter turnout across years in LokSabha',
+      title: 'Parties Contested and Represented across years in LokSabha',
       xaxis: {
         title: 'Year(Assembly Number)'
       },
       yaxis:{
-        title: 'Turnout in %'
+        title: 'Number of Parties'
       }
     };
     let config = {

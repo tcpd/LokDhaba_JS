89f1977a9d7e1238246b72ee18055db1554f120c
saityusuf <yusuf@TCPDMac.local>
2019-11-18 17:01:22 +0530

    Reduce footer size and make its z-axis -1

--- lokdhaba_js/src/Components/Shared/Footer.js
+++ lokdhaba_js/src/Components/Shared/Footer.js
@@ -9,14 +9,14 @@ var style = {
   position: "fixed",
   left: "0",
   bottom: "0",
-  height: "80px",
+  height: "60px",
   width: "100%",
 }
 
 var phantom = {
 display: 'block',
 padding: '20px',
-height: '60px',
+height: '30px',
 width: '100%',
 }
 

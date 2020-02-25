19c41725ddd9273b70d1502ad6b8ec33ccf224c7
Mohit Kumar <mohitkumar@Mohits-MacBook-Pro-2.local>
2020-02-25 13:46:20 +0530

    added google analytics to lokdhaba js

--- lokdhaba_js/src/index.js
+++ lokdhaba_js/src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.js'
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+import ReactGA from 'react-ga';
+
 
 import './index.css'
 import App from './App.js';
+
+const trackingId = "UA-159026500-1"; // Replace with your Google Analytics tracking ID
+ReactGA.initialize(trackingId);
 
 ReactDOM.render(
   <Router>

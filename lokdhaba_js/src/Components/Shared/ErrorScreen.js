edc385dd6ea0149fb4b23fdfb0b58f975860d783
Karnav <karnavpopat@gmail.com>
2022-01-02 12:31:49 +0530

    chore: indentation in ErrorScreen.js

--- lokdhaba_js/src/Components/Shared/ErrorScreen.js
+++ lokdhaba_js/src/Components/Shared/ErrorScreen.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import '../../Assets/Styles/select.css';
 
 const ErrorScreen = (props) => {
-	return (
-		<div style={{color: "red", display: "flex", justifyContent: "center", alignItems: "center", height: "100%", textAlign: "center"}}>
-        	{props.message}
-        	<br/>
-        	Please refresh the page and try again.
-        </div>
-	)
+  return (
+    <div style={{color: "red", display: "flex", justifyContent: "center", alignItems: "center", height: "100%", textAlign: "center"}}>
+      {props.message}
+      <br/>
+      Please refresh the page and try again.
+    </div>
+  )
 }
 
 export default ErrorScreen;

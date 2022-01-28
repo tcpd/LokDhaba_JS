1bc0427762a40d785a75cf93e241dc945ddc204c
Mohit Kumar <mohit.kumar@ashoka.edu.in>
2022-01-28 16:45:06 +0530

    ordered traces and legends alphabetically for profession and level wise for education charts

--- lokdhaba_js/src/Components/Shared/PartyStepBarChart.js
+++ lokdhaba_js/src/Components/Shared/PartyStepBarChart.js
@@ -8,6 +8,13 @@ export default class PartyStepBarChart extends Component {
   render() {
     const { layout, vizParameter, showAdditionalText, getAdditionalText, varName  } = this.props;
     var vizData = this.props.data;
+    if(varName ==="MyNeta_education"){
+      var eduClasses = {"Literate":1,"5th Pass":2,"8th Pass":3,"10th Pass":4,"12th Pass":5,"Graduate":6, "Graduate Professional":7,"Post Graduate":8,"Doctorate":9,"Illiterate":10,"Others":11,"Not Given":12,"Unknown":13,"":14};
+      vizData = vizData.sort((a,b) => (eduClasses[a[varName]] > eduClasses[b[varName]]) ? -1 : ((eduClasses[b[varName]] > eduClasses[a[varName]]) ? 1 : 0));
+    }else{
+      vizData= vizData.sort((a,b) => (a[varName] > b[varName]) ? -1 : ((b[varName] > a[varName]) ? 1 : 0));
+    }
+
     var data = [];
     var categories = new Set(vizData.map(x => x[varName]));
     var x_axis_labels = vizData.map(function (item) { return item.Party});
--- lokdhaba_js/src/Components/Shared/PieChart.js
+++ lokdhaba_js/src/Components/Shared/PieChart.js
@@ -7,6 +7,12 @@ export default class PieChart extends Component {
   render() {
     const { layout, vizParameter, varName } = this.props;
     var vizData = this.props.data;
+    if(varName ==="MyNeta_education"){
+      var eduClasses = {"Literate":1,"5th Pass":2,"8th Pass":3,"10th Pass":4,"12th Pass":5,"Graduate":6, "Graduate Professional":7,"Post Graduate":8,"Doctorate":9,"Illiterate":10,"Others":11,"Not Given":12,"Unknown":13,"":14};
+      vizData = vizData.sort((a,b) => (eduClasses[a[varName]] > eduClasses[b[varName]]) ? 1 : ((eduClasses[b[varName]] > eduClasses[a[varName]]) ? -1 : 0));
+    }else{
+      vizData= vizData.sort((a,b) => (a[varName] > b[varName]) ? 1 : ((b[varName] > a[varName]) ? -1 : 0));
+    }
     var vals = vizData.map(function (item) { return item[vizParameter]  });
     var labs = vizData.map(function (item) { return item[varName]  });
     var data = [{
@@ -14,7 +20,8 @@ export default class PieChart extends Component {
       labels: labs,
       textinfo: 'percent',
       hole: .4,
-      type:'pie'
+      type:'pie',
+      sort:false
       //,insidetextorientation: "radial"
     }];
 

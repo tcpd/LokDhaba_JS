a19847d02adac9fbe4f877eee73c4704731ac9f2
Shivam Gangwani <shivam.gangwani2804@gmail.com>
2022-07-01 20:10:54 +0530

    Fixed setup.sh - code was breaking Maps charts

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
-rm -r LokdhabaApi/Maps
-if [ -d "LokdhabaApi/Maps" ]; then rm -Rf LokdhabaApi/Maps; fi
-mkdir LokdhabaApi/Maps
-mkdir LokdhabaApi/Maps/GE
-mkdir LokdhabaApi/Maps/AE
-cp -r  <tcpd_data git repository location>/GE/Maps/Delim4/* LokdhabaApi/Maps/GE/
-cp -r  <tcpd_data git repostitory location>/AE/Maps/Delim4/* LokdhabaApi/Maps/AE/
-
+# rm -r LokdhabaApi/Maps
+# if [ -d "LokdhabaApi/Maps" ]; then rm -Rf LokdhabaApi/Maps; fi
+# mkdir LokdhabaApi/Maps
+# mkdir LokdhabaApi/Maps/GE
+# mkdir LokdhabaApi/Maps/AE
+# cp -r  <tcpd_data git repository location>/GE/Maps/Delim4/* LokdhabaApi/Maps/GE/
+# cp -r  <tcpd_data git repostitory location>/AE/Maps/Delim4/* LokdhabaApi/Maps/AE/
+# -------------------------------
+# CODE REMOVED. 
+# It breaks the Maps charts in ld_ui 
 
 docker stop ld_ui ld_api ld_db
 docker rm ld_ui ld_api ld_db
